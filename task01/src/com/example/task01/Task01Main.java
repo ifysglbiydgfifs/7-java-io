336a9b996e9204ff273c2835edbaa2840434a194
ifysglbiydgfifs <igbsh43hsdh@gmail.com>
2024-11-15 07:00:31 +0500

    Task01

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,5 +1,7 @@
 package com.example.task01;
 
+import java.awt.*;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,14 +10,27 @@ public class Task01Main {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
 
-        /*
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
+        System.out.println(checkSumOfStream(null));
 
     }
 
+    public static final String ANSI_MAGENTA = "\u001B[35m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
     public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+        try {
+            int sum = 0;
+            int input = inputStream.read();
+
+            while (input != -1) {
+                sum = Integer.rotateLeft(sum, 1) ^ input;
+                input = inputStream.read();
+            }
+            return sum;
+        } catch (NullPointerException exception) {
+            System.out.println(ANSI_MAGENTA + "Bytes array is empty " + exception + ANSI_RESET);
+            throw new IllegalArgumentException(exception);
+        }
     }
 }
